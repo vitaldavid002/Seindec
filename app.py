b3350cfebe8a667334f83a34dc68f586b9827548
vitaldavid002 <vitaldavid002@gmail.com>
2026-05-18 22:00:07 -0300

    Adjust column widths in layout configuration

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ if not st.session_state.logado:
     # Se não tem cookie ou sessão expirou, mostra formulário
     st.set_page_config(layout="centered")
 
-    col1, col2, col3 = st.columns([1, 3, 1])
+    col1, col2, col3 = st.columns([2, 3, 2])
     with col2:
         st.image("assets/logo_login2.png", use_container_width=True)
 
